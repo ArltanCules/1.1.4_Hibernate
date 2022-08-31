85d1cb282aaa4d88055b7f03efc5220437457c1f
ArltanCules <106382293+ArltanCules@users.noreply.github.com>
2022-08-31 12:30:15 +0300

    константы

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -20,13 +20,18 @@ import java.util.Properties;
 
 
 public class Util {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/mysql";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+    private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
     private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection=null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root", "root");
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(HOST,LOGIN, PASSWORD);
             System.out.println("Connection ok");
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -40,11 +45,11 @@ public class Util {
                 try {
                     Configuration configuration = new Configuration();
                     Properties settings = new Properties();
-                    settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mysql");
-                    settings.put(Environment.USER, "root");
-                    settings.put(Environment.PASS, "root");
-                    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                    settings.put(Environment.DRIVER, DRIVER);
+                    settings.put(Environment.URL, HOST);
+                    settings.put(Environment.USER, LOGIN);
+                    settings.put(Environment.PASS, PASSWORD);
+                    settings.put(Environment.DIALECT,DIALECT );
                     settings.put(Environment.SHOW_SQL, "true");
                     settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
                     settings.put(Environment.HBM2DDL_AUTO, "update");
